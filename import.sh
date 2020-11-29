83ada208dfdaf07983b86eca743987ad231c1795
Dave McKinstry <dmckinstry@github.com>
2020-11-29 13:26:05 -0700

    Update script

--- import.sh
+++ import.sh
@@ -6,17 +6,25 @@ if [ -z $GITBUCKET_REPO ]; then GITBUCKET_REPO="test1"; fi
 if [ -z $GITHUB_SERVICE ]; then GITHUB_SERVICE="https://github.com"; fi
 if [ -z $GITHUB_OWNER ]; then GITHUB_OWNER="dmckinstry"; fi
 
+SOURCE_REPO=$GITBUCKET_SERVER/git/$GITBUCKET_OWNER/$GITBUCKET_REPO.git
+DEST_REPO=$GITHUB_SERVICE/$GITHUB_OWNER/$GITBUCKET_REPO.git
+echo Migrate $SOURCE_REPO to $DEST_REPO
 
-md tmp
+mkdir tmp
 cd tmp
-SOURCE_REPO=$GITBUCKET_SERVER+"/git/"+$GITBUCKET_OWNER+"/"$GITBUCKET_REPO+".git"
+
+# Create repo...  This will initialize the current folder but we're not going to use it
+gh repo create --confirm --private $GITHUB_OWNER/$GITBUCKET_REPO
+
+# Clone the source into a subfolder
 git clone $SOURCE_REPO
 cd $GITBUCKET_REPO
 
-gh repo create --private $GITHUB_OWNER+"/"+$GITBUCKET_REPO
-git status
-git remote set-url origin $GITHUB_SERVICE+"/"+$GITHUB_OWNER+"/"+$GITBUCKET_REPO+".git"
+# Import it into the new GitHub repo
+git remote set-url origin $DEST_REPO
 git push
 
-#cd ../..
-#rm -rf tmp
+# Clean up
+cd ..
+cd ..
+rm -rf tmp
